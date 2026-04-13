98f5a90742c5c9661ab54044466da187e3ea349f
Arindam B <arindamcodes394@gmail.com>
2026-04-13 15:11:58 +0530

    series 1D

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# series: 1 D array that can hold any data type
+
+
+# calories = {"day 1": 1700, "day 2": 1950, "day 3": 2200 }
+
+# series = pd.Series(calories)
+
+# print(series[series >= 2000])
+
+# series.loc["day 3"] += 500
+# print(series)
+
+# print(series.iloc[2])
